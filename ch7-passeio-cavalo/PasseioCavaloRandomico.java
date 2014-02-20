69aa9db5a1a71086648002644b0ed9b62f2cce89
Ricidleiv Tondatto <tondatto@gmail.com>
2014-02-19 20:11:51 -0400

    renomeando a PasseioCavalo

---
+++ ch7-passeio-cavalo/PasseioCavaloRandomico.java
@@ -0,0 +1,111 @@
+import java.util.Random;
+import java.util.Set;
+import java.util.HashSet;
+
+public class PasseioCavaloRandomico {
+	private Tabuleiro tabuleiro;
+	private Random randomNumber = new Random();
+	
+	public PasseioCavaloRandomico (int posx, int posy){
+		tabuleiro = new Tabuleiro(posx, posy);
+	}
+	
+	public void run(){		
+		int moveNumber; 
+		Set<Integer> tentativas = new HashSet<>();
+		
+		while(!tabuleiro.isFinished()){
+			do {
+				moveNumber = randomNumber.nextInt(8);
+			} while (tentativas.contains(moveNumber));
+			
+			if(tabuleiro.isMovimentoValido(moveNumber)){
+				tabuleiro.setMovimento(moveNumber);
+				tentativas.clear();
+			} else {
+				tentativas.add(moveNumber);
+			}
+		}
+		
+		tabuleiro.imprimeTabuleiro();
+	}
+	
+	private class Tabuleiro {
+		private final int[] HORIZONTAL = { 2,  1, -1, -2, -2, -1, 1, 2};
+		private final int[] VERTICAL   = {-1, -2, -2, -1,  1,  2, 2, 1};
+		
+		int[][] tabuleiro = {{0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0},
+		                     {0,0,0,0,0,0,0,0}};
+		
+		int linhaCorrente;
+		int colunaCorrente;
+		
+		int counter = 0;
+		
+		public Tabuleiro (int posx, int posy){
+			if (posx >= 1 && posx <= 8 && posy >= 1 && posy <= 8){
+				linhaCorrente = posx - 1;
+				colunaCorrente = posy - 1;
+				
+				tabuleiro[linhaCorrente][colunaCorrente] = ++counter;
+				
+				//logPosicao();
+			} else {
+				throw new IllegalArgumentException("Posições devem ser entre 1 e 8.");
+			}
+		}
+		
+		public void setMovimento(int idx){
+			linhaCorrente += VERTICAL[idx];
+			colunaCorrente += HORIZONTAL[idx];
+			
+			//System.out.printf("\nh: %d, v: %d\n",HORIZONTAL[idx], VERTICAL[idx]);
+			
+			//logPosicao();
+			
+			tabuleiro[linhaCorrente][colunaCorrente] = ++counter;
+			
+			//imprimeTabuleiro();
+		}
+		
+		public boolean isMovimentoValido(int idx){
+			return (colunaCorrente + HORIZONTAL[idx] >= 0 && colunaCorrente + HORIZONTAL[idx] < 8)
+				&& (linhaCorrente + VERTICAL[idx] >= 0 && linhaCorrente + VERTICAL[idx] < 8)
+				&& (tabuleiro[linhaCorrente + VERTICAL[idx]][colunaCorrente + HORIZONTAL[idx]] == 0);
+		}
+		
+		public boolean isFinished(){
+			return counter == 64 || !haMovimentoLivre();
+		}
+		
+		public boolean haMovimentoLivre(){
+			for(int i=0; i < 8; i++){
+				if(isMovimentoValido(i)){
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		public void imprimeTabuleiro(){
+			System.out.printf("\nTotal de movimentos: %d.\n", counter);
+			
+			for(int i=0; i < 8; i++){
+				for(int j=0; j< 8; j++){
+					System.out.printf("%4s", (tabuleiro[i][j] == 0) ? "-" : tabuleiro[i][j]);
+				}
+				System.out.println();
+			}
+		}
+		
+		public void logPosicao(){
+			System.out.printf("#%d: %d, %d\n", counter, linhaCorrente, colunaCorrente);
+		}
+	}
+}
